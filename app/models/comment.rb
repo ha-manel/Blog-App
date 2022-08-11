ab0316965af8526ec425fab5e464d5a337e36e6b
ha-manel <manelhammouche3@gmail.com>
2022-08-11 20:11:22 +0100

    Add methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post, counter_cache: true
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def update_comments_counter
+    post.increment!(:comments_count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :post, counter_cache: true
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  def update_likes_counter
+    post.increment!(:likes_count)
+  end
 end

3298cf8b8340609b12583d93ed66678b6ea465d1
ha-manel <manelhammouche3@gmail.com>
2022-08-12 14:51:27 +0100

    Add foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)

afccaa49bbbf78ed9bc496ca50fcecccb8336ef7
ha-manel <manelhammouche3@gmail.com>
2022-08-12 12:48:07 +0100

    Add model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
   has_many :likes
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :posts
   has_many :comments
   has_many :likes

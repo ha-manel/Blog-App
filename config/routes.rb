cc34774df65f7eaace32cfd08a7eb94549280cb1
Lembani <Lembanisakala@gmail.com>
2022-08-26 11:40:33 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
       resources :likes, only: [:create, :destroy]
     end
   end
+  get 'users/:user_id/posts_list', to: 'api#list_all_user_posts', as: :list_all_user_posts
+  get 'users/:user_id/posts/:post_id/comments_list', to: 'api#list_all_post_comments', as: :list_all_post_comments
+  post 'users/:user_id/posts/:post_id/add_comment', to: 'api#add_comment', as: :add_comment
 end
